bfa00620408598a73f36e7aac9efdbea34c71a04
unknown <omidasgary966@gmail.com>
2024-06-13 10:01:30 +0330

    edit Team2

--- database/migrations/2024_03_24_105012_create_orders_table.php
+++ database/migrations/2024_03_24_105012_create_orders_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->bigInteger('phone_number');
+            $table->string('phone_number');
             $table->string('Address');
             $table->string('products');
             $table->string('number');
